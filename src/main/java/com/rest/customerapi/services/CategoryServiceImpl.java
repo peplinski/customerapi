bf227840063864fb2504c7b0c873db7629416990
Driver <peplinskikrzysztof@gmail.com>
2018-10-24 18:14:13 +0200

    controller

--- src/main/java/com/rest/customerapi/services/CategoryServiceImpl.java
+++ src/main/java/com/rest/customerapi/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.rest.customerapi.services;
 import com.rest.customerapi.api.v1.mapper.CategoryMapper;
 import com.rest.customerapi.api.v1.model.CategoryDTO;
 import com.rest.customerapi.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryMapper categoryMapper;
     private final CategoryRepository categoryRepository;
