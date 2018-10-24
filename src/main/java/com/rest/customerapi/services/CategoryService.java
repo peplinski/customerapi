8453d3ed27b55f9ef61e90e99470d77ec192a9fe
Driver <peplinskikrzysztof@gmail.com>
2018-10-24 18:04:11 +0200

    service

---
+++ src/main/java/com/rest/customerapi/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.rest.customerapi.services;
+
+import com.rest.customerapi.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
