b9cc829adee7a61f4d2b9e79b45d2b786411d4a6
Driver <peplinskikrzysztof@gmail.com>
2018-10-27 11:50:43 +0200

    Itegration test with DataJPATest

--- src/main/java/com/rest/customerapi/services/CustomerServiceImpl.java
+++ src/main/java/com/rest/customerapi/services/CustomerServiceImpl.java
@@ -13,18 +13,13 @@ import java.util.stream.Collectors;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerMapper customerMapper;
-    private CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    private final CustomerRepository customerRepository;
 
-   @Autowired
-   public void setCustomerMapper(CustomerMapper customerMapper){
-       this.customerMapper=customerMapper;
-   }
-
-   @Autowired
-   public void setCustomerRepository(CustomerRepository customerRepository){
-       this.customerRepository=customerRepository;
-   }
+    public CustomerServiceImpl(CustomerMapper customerMapper, CustomerRepository customerRepository) {
+        this.customerMapper = customerMapper;
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
@@ -77,5 +72,21 @@ public class CustomerServiceImpl implements CustomerService {
         customer.setId(id);
 
         return saveAndReturnDTO(customer);
+    }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstName() != null){
+                customer.setFirstName(customerDTO.getFirstName());
+            }
+
+            if(customerDTO.getLastName() != null){
+                customer.setLastName(customerDTO.getLastName());
+            }
+
+            return customerMapper.customerTOCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
     }
 }
--- src/test/java/com/rest/customerapi/services/CustomerServiceImplTest.java
+++ src/test/java/com/rest/customerapi/services/CustomerServiceImplTest.java
@@ -30,10 +30,8 @@ public class CustomerServiceImplTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        customerService=new CustomerServiceImpl();
-        customerService.setCustomerMapper(customerMapper);
-        customerService.setCustomerRepository(customerRepository);
-        // customerService = new CustomerServiceImpl(customerMapper, customerRepository);
+
+        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
     }
 
     @Test
