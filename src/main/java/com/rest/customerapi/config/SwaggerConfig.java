37af987674aeee43f38800e219e880dfff6ed3bb
Driver <peplinskikrzysztof@gmail.com>
2018-11-03 10:41:10 +0100

    swagger metaData

--- src/main/java/com/rest/customerapi/config/SwaggerConfig.java
+++ src/main/java/com/rest/customerapi/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,7 +23,24 @@ public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Krzysztof Pepliński", "https://www.linkedin.com/in/kpeplinski",
+                "peplinskikrzysztof@gmail.com");
+
+        return new ApiInfo(
+                "Przykład api",
+                "Spring Framework 5: api example",
+                "1.0",
+                "Terms of Service: bla bla bla",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 
 //    @Override
